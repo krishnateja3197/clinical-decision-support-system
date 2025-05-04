24904d6a0cc6a55a845d318c5fc2356bc55823f1
krishnateja3197 <128712035+krishnateja3197@users.noreply.github.com>
2025-05-04 21:56:59 +0530

    Add files via upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+function Home() {
+  return (
+    <div
+      className="w-full h-screen bg-[url('https://wallpapers.com/images/featured/healthcare-oco8w27tkw40cp90.webp')] bg-cover bg-center flex items-start justify-center pt-[520px] pr-[1320px]"
+    >
+      <span className="text-white text-6xl font-opensans leading-[1.8] tracking-[2px]">
+        We care
+        <br />
+        for your health
+      </span>
+
+
+    </div>
+  );
+}
+
+export default Home;
